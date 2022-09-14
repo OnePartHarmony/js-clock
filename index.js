68c959fe7a37786078ecbcd74b4d8f168e2e0961
OnePartHarmony <bakingharmony@gmail.com>
2022-09-13 23:21:48 -0400

    Satisfied that this is a working clock, will come back to it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const startButton = document.getElementById("startButton")
 const stopButton = document.getElementById("stopButton")
-const clockButton = document.getElementById("clockButton")
 const faceContainer = document.getElementById("faceContainer")
 
 //this function creates a clock face
@@ -22,16 +22,49 @@ const makeClock = () => {
     clockFace.appendChild(secondHand)
 }
 
-//make second hand rotate per second
-
-//make minute hand rotate per minute
-
-//make hour hand rotate per hour
 
 
+//functions to determine to what position each hand should be rotated each second
+const secondRotation = (second) => {
+    return (second / 60) * 360
+}
+
+const minuteRotation = (second) => {
+    return ((second / 60) /60) *360
+}
+
+const hourRotation = (second) => {
+    return (((second / 60) /60) /60) *360
+}
+
+//function rotates each hand per second (-1.8deg just to match clock image better)
+let secondCount = 1
+const rotPerSecond = () => {
+    if (secondCount === 360){
+        secondCount = 0
+    }
+    let secondDeg = secondRotation(secondCount) - 1.8
+    let minuteDeg = minuteRotation(secondCount) - 1.8
+    let hourDeg = hourRotation(secondCount) - 1.8
+
+    document.getElementById("secondHand").style.transform = "rotate(" + secondDeg + "deg)"
+    document.getElementById("minuteHand").style.transform = "rotate(" + minuteDeg + "deg)"
+    document.getElementById("hourHand").style.transform = "rotate(" + hourDeg + "deg)"
+    secondCount++
+}
+
+//A function that makes the clock and starts it rotating per second
+const startClock = () => {
+    makeClock()
+    window.clockInterval = setInterval(rotPerSecond, 1000)
+}
 
 
+startButton.addEventListener("click", startClock, { once: true })
 
+const stopClock = () => {
+    clearInterval(clockInterval)
+}
 
-stopButton.addEventListener("click", makeClock, { once: true })
+stopButton.addEventListener("click", stopClock, { once: true })
 
