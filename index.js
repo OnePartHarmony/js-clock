352a877a4bbbea2b7e20e07b5c4b93bd5e547974
OnePartHarmony <bakingharmony@gmail.com>
2022-09-13 21:56:03 -0400

    added in some images of hands - I can't believe the css is keeping me from this js project.

--- index.js
+++ index.js
@@ -8,38 +8,20 @@ const makeClock = () => {
     const clockFace = document.createElement("div")
     clockFace.setAttribute("id", "clockFace")
     faceContainer.appendChild(clockFace)
-    console.log(clockFace)
-//create numbers
-    let twelve = document.createElement("div")
-    let three = document.createElement("div")
-    let six = document.createElement("div")
-    let nine = document.createElement("div")
-    twelve.setAttribute("id", "twelve")
-    twelve.classList.add("clockNumber")
-    three.setAttribute("id", "three")
-    three.classList.add("clockNumber")
-    six.setAttribute("id", "six")
-    six.classList.add("clockNumber")
-    nine.setAttribute("id", "nine")
-    nine.classList.add("clockNumber")
-    twelve.innerText = "12"
-    three.innerText = "3"
-    six.innerText = "6"
-    nine.innerText = "9"
-    clockFace.appendChild(twelve)
-    clockFace.appendChild(three)
-    clockFace.appendChild(six)
-    clockFace.appendChild(nine)
-
 //create hour hand
-
+    const hourHand = document.createElement("div")
+    hourHand.setAttribute("id", "hourHand")
+    clockFace.appendChild(hourHand)
 //create minute hand
-
+    const minuteHand = document.createElement("div")
+    minuteHand.setAttribute("id", "minuteHand")
+    clockFace.appendChild(minuteHand)
 //create second hand
-
-
+    const secondHand = document.createElement("div")
+    secondHand.setAttribute("id", "secondHand")
+    clockFace.appendChild(secondHand)
 }
 
 
-clockButton.addEventListener("click", makeClock, { once: true })
+stopButton.addEventListener("click", makeClock, { once: true })
 
